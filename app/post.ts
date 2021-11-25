467b107c4fa8176692e691396be057314a796a45
Maciek <msitkowski94@gmail.com>
2021-11-25 19:27:29 +0100

    handle adding a new post

--- app/post.ts
+++ app/post.ts
@@ -1,8 +1,8 @@
-import path from "path";
-import fs from "fs/promises";
-import parseFrontMatter from "front-matter";
-import invariant from "tiny-invariant";
-import { marked } from "marked";
+import path from 'path';
+import fs from 'fs/promises';
+import parseFrontMatter from 'front-matter';
+import invariant from 'tiny-invariant';
+import { marked } from 'marked';
 
 export type Post = {
   slug: string;
@@ -15,41 +15,54 @@ export type PostMarkdownAttributes = {
 };
 
 function isValidPostAttributes(
-  attributes: any
+  attributes: any,
 ): attributes is PostMarkdownAttributes {
   return attributes?.title;
 }
 
 // relative to the server output not the source!
-const postsPath = path.join(__dirname, "..", "posts");
+const postsPath = path.join(__dirname, '..', 'posts');
 
 export async function getPosts() {
   const dir = await fs.readdir(postsPath);
 
   return Promise.all(
-    dir.map(async (fileName) => {
+    dir.map(async fileName => {
       const file = await fs.readFile(path.join(postsPath, fileName));
       const { attributes } = parseFrontMatter(file.toString());
       invariant(
         isValidPostAttributes(attributes),
-        `${fileName} has bad meta data!`
+        `${fileName} has bad meta data!`,
       );
 
       return {
-        slug: fileName.replace(/.md$/, ""),
+        slug: fileName.replace(/.md$/, ''),
         title: attributes.title,
       };
-    })
+    }),
   );
 }
 
 export async function getPost(slug: string): Promise<Post> {
-  const filepath = path.join(postsPath, slug + ".md");
+  const filepath = path.join(postsPath, slug + '.md');
   const file = await fs.readFile(filepath);
   const { attributes, body } = parseFrontMatter(file.toString());
   invariant(
     isValidPostAttributes(attributes),
-    `Post ${filepath} is missing attributes`
+    `Post ${filepath} is missing attributes`,
   );
   return { slug, title: attributes.title, html: marked(body) };
+}
+
+type NewPost = {
+  title: string;
+  markdown: string;
+};
+
+export async function createPost(post: NewPost) {
+  let md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+  let slug = post.title.toLowerCase().replace(/\s+/g, '-');
+
+  await fs.writeFile(path.join(postsPath, slug + '.md'), md);
+  return getPost(slug);
 }
--- app/routes/admin/new.tsx
+++ app/routes/admin/new.tsx
@@ -1,3 +1,53 @@
+import { Form, redirect, useActionData, useTransition } from 'remix';
+import { createPost } from '~/post';
+import type { ActionFunction } from 'remix';
+import invariant from 'tiny-invariant';
+
+export let action: ActionFunction = async ({ request }) => {
+  // Fake delay
+  await new Promise(res => setTimeout(res, 2000));
+
+  let formData = await request.formData();
+  let title = formData.get('title');
+  let markdown = formData.get('markdown');
+
+  let errors: Record<string, boolean> = {};
+  if (!title) errors.title = true;
+  if (!markdown) errors.markdown = true;
+
+  if (Object.keys(errors).length) {
+    return errors;
+  }
+
+  invariant(typeof title === 'string');
+  invariant(typeof markdown === 'string');
+  await createPost({ title, markdown });
+
+  return redirect('/admin');
+};
+
 export default function NewPost() {
-  return <h2>New Post</h2>;
+  let errors = useActionData();
+  let transition = useTransition();
+
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title: {errors?.title && <em>Title is required</em>}{' '}
+          <input type="text" name="title" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">Markdown:</label>{' '}
+        {errors?.markdown && <em>Markdown is required</em>}{' '}
+        <textarea id="markdown" name="markdown" rows={20} />
+      </p>
+      <p>
+        <button type="submit">
+          {transition.submission ? 'Creating...' : 'Create Post'}
+        </button>
+      </p>
+    </Form>
+  );
 }
